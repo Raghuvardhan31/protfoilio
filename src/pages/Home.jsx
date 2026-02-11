332693e77df51024b3e1d358e994613d17fcebff
Raghuvardhan <bonjuriraghuvardhan@gmail.com>
2026-02-11 15:06:57 +0530

    correct main first page image and description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
-    <div>
-      <h1>  Home page content goes here.</h1>
-    
+    <div className="home">
+      <div className="overlay">
+        <div className='desc'>
+          <h2>Welcome to My Portfolio</h2>
+          <h3>Hi, I’m Raghuvardhan Bonjuri</h3>
+          <p>
+            Full Stack Developer specializing in building scalable web 
+            applications using React, Django, Spring Boot, Node.js, and AWS. 
+            I design clean, secure RESTful APIs and develop production-ready 
+            backend systems with a strong focus on performance and architecture. 
+            Passionate about cloud technologies, backend engineering, and AI-driven 
+            applications including LLMs and RAG pipelines.
+          </p>  
+        </div>
+
+        <div className='img'>
+          <img src="raghu.png" alt="Raghuvardhan Bonjuri" />
+        </div>
+      </div>
     </div>
   )
 }
